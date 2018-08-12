70af477486e2861e155e702e70e0f0309534741d
Charles Samborski <demurgos@demurgos.net>
2018-08-13 01:30:01 +0200

    Stub local wrapper API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {applyContextOnGlobal, legacyWrap, runMain, wrapGlobal} = require('./lib/index')
+const {withSpawnWrap, withSpawnWrapSync} = require('./lib/local')
 
 module.exports = legacyWrap
-Object.assign(module.exports, {applyContextOnGlobal, runMain, wrapGlobal})
+Object.assign(module.exports, {applyContextOnGlobal, runMain, wrapGlobal, withSpawnWrap, withSpawnWrapSync})
