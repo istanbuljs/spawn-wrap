80a4113e9a4a16ece9d232b1d5de777e4926357c
Charles Samborski <demurgos@demurgos.net>
2018-08-15 18:32:08 +0200

    Move source files to `src` directory

--- index.js
+++
@@ -1,5 +0,0 @@
-const {applyContextOnGlobal, legacyWrap, runMain, wrapGlobal} = require('./lib/index')
-const {withSpawnWrap, withSpawnWrapSync} = require('./lib/local')
-
-module.exports = legacyWrap
-Object.assign(module.exports, {applyContextOnGlobal, runMain, wrapGlobal, withSpawnWrap, withSpawnWrapSync})
